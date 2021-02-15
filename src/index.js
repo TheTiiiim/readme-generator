a334badecc08306cda85f1fca7f5d49bd05358ca
Tim Scott <0timothy.scott0@gmail.com>
2021-02-15 09:19:28 -0500

    writeToFile creates directory if it doesnt exist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 const inquirer = require("inquirer");
-const fsPromise = require("fs").promises;
+const path = require("path");
+const fs = require("fs");
+const { dirname } = require("path");
+const fsPromises = fs.promises;
 
+`
+project title, description, installation instructions, usage information, contribution guidelines, and test instructions
+
+`
+
+const questions = [];
+
+function writeToFile(fileName, data) {
+	fileName = path.normalize(fileName);
+
+	// get directory of target file
+	dirName = fileName.split(path.sep);
+	dirName.pop();
+	dirName = dirName.join(path.sep);
+
+	// ensure directory exists
+	if (!fs.existsSync(dirName)) {
+		fs.mkdirSync(dirName);
+	}
+
+	// write file
+	return fsPromises.writeFile(fileName, data, "utf8");
+}
+
+function init() {
+}
+
+init();
